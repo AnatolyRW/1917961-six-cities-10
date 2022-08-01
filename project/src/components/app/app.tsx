628db3b2bf316ac5099f0d4ff917fb0ba239db10
Anatoly Ryzhkov <Ryzhkov.Anatoly@gmail.com>
2022-08-01 20:26:33 +0500

    Создал страницу Not Found 3.9. Маршрут по тайным тропам

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,9 +1,21 @@
+import {Route, BrowserRouter, Routes} from 'react-router-dom';
 import Main from '../../pages/main/main';
+import Login from '../../pages/login/login';
+import Favorites from '../../pages/favorites/favorites';
+import NotFound from '../../pages/not-found/not-found';
 import MainPageProps from '../../types/main-page-props';
 
 function App({placeCount} : MainPageProps): JSX.Element {
   return(
-    <Main placeCount={placeCount}/>
+    //<Main placeCount={placeCount}/>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={}
+          element={}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- project/src/const.ts
+++ project/src/const.ts
@@ -1,3 +1,12 @@
 const PLACE_COUNT = 10;
 
 export default PLACE_COUNT;
+
+export enum AppRoute {
+  Login = '/login',
+  Lose = '/lose',
+  Result = '/result',
+  Root = '/',
+  DevArtist = '/dev-artist',
+  DevGenre = '/dev-genre'
+}
